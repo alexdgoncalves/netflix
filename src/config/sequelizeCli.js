3f619ffa141bcb2f6d29050a9e809aece664eded
alexdgoncalves <alex.d.goncalves21@gmail.com>
2022-06-17 11:39:27 -0300

    sequelize config

---
+++ src/config/sequelizeCli.js
@@ -0,0 +1,12 @@
+// src/config/sequelizeCli.js
+
+module.exports = {
+  development: {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: '5432',
+    database: 'netflix_development',
+    username: 'alexdgoncalves',
+    password: 'alexdgoncalves'
+  }
+}
